53bc9c3e3d95f8381248e5c45c3d972586c710db
lukas.krobath <lukas.krobath@yahoo.at>
2019-11-19 14:00:58 +0100

    Change beans classes

--- src/main/java/beans/Publisher.java
+++ src/main/java/beans/Publisher.java
@@ -5,33 +5,19 @@
  */
 package beans;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author annalechner
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Publisher {
     
     private String name;
     private String url;
-
-    public Publisher(String name, String url) {
-        this.name = name;
-        this.url = url;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
